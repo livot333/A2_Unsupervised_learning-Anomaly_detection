b85b893e8411cd915ef932cce31d1d51bdca41bc
livot333 <livortom@gmail.com>
2026-02-26 20:50:03 +0100

    correlation calculation

--- Main.py
+++ Main.py
@@ -4,9 +4,12 @@ train_path = r"D:\SKOLA\NTNU\MLL\Assingment_2\Dataset_file\data\data\test"
 test_path = r"D:\SKOLA\NTNU\MLL\Assingment_2\Dataset_file\data\data\train"
 result_file_path = r"D:\SKOLA\NTNU\MLL\Assingment_2\Dataset_file\labeled_anomalies.csv"
 
+correlation_outfile_path = r"D:\SKOLA\NTNU\MLL\Assingment_2"
+
 
 dt = DatasetOperations(test_path=test_path,train_path=train_path,results_path=result_file_path)
 
 train_dataset, test_dataset , results = dt.load_data()
 dt.dataset_info()
 dt_plt = dt.plot_data(choosen_dataset="training_dataset",start_channel_id="A1")
+correlation_dictionary = dt.correlation_check(mode="training_dataset",correlation_outfile_path=correlation_outfile_path)
